e2a0010b79c282eaee7bfe07607812d989a5504b
Jorge Fuerte <fuerte1206@gmail.com>
2023-11-26 19:50:40 -0500

    Finished macropad code

--- Macropad/Macropad.ino
+++ Macropad/Macropad.ino
@@ -3,30 +3,18 @@
 #include <ClickEncoder.h>
 #include <TimerOne.h>
 
-#define KEY_F13   0xF0
-#define KEY_F14   0xF1
-#define KEY_F15   0xF2
-#define KEY_F16   0xF3
-#define KEY_F17   0xF4
-#define KEY_F18   0xF5
-#define KEY_F19   0xF6
-#define KEY_F20   0xF7
-#define KEY_F21   0xF8
-#define KEY_F22   0xF9
-#define KEY_F23   0xFA
-#define KEY_F24   0xFB
+const byte ROWS = 4; 
+const byte COLS = 3; 
 
-const byte ROWS = 3; 
-const byte COLS = 4; 
-
-int hexaKeys[ROWS][COLS] = {
-  {KEY_F13,KEY_F14,KEY_F15,KEY_F16},
-  {KEY_F17,KEY_F18,KEY_F19,KEY_F20},
-  {KEY_F21,KEY_F22,KEY_F23,KEY_F24}
+char hexaKeys[ROWS][COLS] = {
+  {'0','1','2'},
+  {'4','5','6'},
+  {'8','9','A'},
+  {'C','D','E'}
 };
 
-byte rowPins[ROWS] = {18,19,20}; 
-byte colPins[COLS] = {15,14,16,10}; 
+byte rowPins[ROWS] = {15,14,16,10}; 
+byte colPins[COLS] = {18,19,20}; 
 
 Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
@@ -56,13 +44,47 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int customKey = customKeypad.getKey();
+  char customKey = customKeypad.getKey();
   valuel += encoderl->getValue();
   valuer += encoderr->getValue();
   
-  if (customKey){
-    Keyboard.press(customKey);
-    delay(10);
+  switch(customKey){
+    case 'C':
+      Keyboard.write(KEY_F13);
+      break;
+    case '8':
+      Keyboard.write(KEY_F14);
+      break;
+    case '4':
+      Keyboard.write(KEY_F15);
+      break;
+    case '0':
+      Keyboard.write(KEY_F16);
+      break;
+    case 'D':
+      Keyboard.write(KEY_F17);
+      break;
+    case '9':
+      Keyboard.write(KEY_F18);
+      break;
+    case '5':
+      Keyboard.write(KEY_F19);
+      break;
+    case '1':
+      Keyboard.write(KEY_F20);
+      break;
+    case 'E':
+      Keyboard.write(KEY_F21);
+      break;
+    case 'A':
+      Keyboard.write(KEY_F22);
+      break;
+    case '6':
+      Keyboard.write(KEY_F23);
+      break;
+    case '2':
+      Keyboard.write(KEY_F24);
+      break;
   }
 
   if (valuel != lastl) {
